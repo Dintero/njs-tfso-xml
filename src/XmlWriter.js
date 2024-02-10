a4a2e2d5c662362498c8076f0ec8ad4a6ffd36c6
Frode Aannevik <frode@dintero.com>
2024-02-10 18:12:57 +0100

    Rebase with upsteam
    - Remove eslint and release build as we will not be creating releases
    
    See: https://github.com/tfso/njs-tfso-xml/commit/89c5809df5f53de3bd0549847136b3cca7fbee0c

--- src/XmlWriter.js
+++ src/XmlWriter.js
@@ -1,7 +1,6 @@
 const DOMImplementation = require('@xmldom/xmldom').DOMImplementation
 const XMLSerializer = require('@xmldom/xmldom').XMLSerializer
 const DOMParser = require('@xmldom/xmldom').DOMParser
-const XmlReader = require('./XmlReader')
 
 class XmlWriter {
     /**
